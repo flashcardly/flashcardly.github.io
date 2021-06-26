c890cfba8650e63c79d6941a801f7d435584a994
Lisa <lisa.lehrman@gmail.com>
2021-06-25 22:38:47 -0700

    flashcard animation progress

--- src/features/flashcard/flashcard.tsx
+++ src/features/flashcard/flashcard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import type { Card } from '../../types/types'
+import "./flashcard.css";
 /*
 
 */
@@ -25,15 +26,9 @@ export const Flashcard = ({ deck }: FlashcardProps): JSX.Element => {
     const card = deck[index] ?? { front: "", back: "" };
     return (
         <div className="scene">
-            <div onClick={flipcard} className="card">
-                <div className={`card__face ${isFront ? '' : 'is-flipped'}`}>
-                    <div className={`card__face--front`}>
-                        <h1>{card.front}</h1>
-                    </div>
-                    <div className={`card__face--back`}>
-                        <h1>{card.back}</h1>
-                    </div>
-                </div>
+            <div onClick={flipcard} className={`card ${isFront ? '' : 'card--is-flipped'}`}>
+                <div className={`card__face card__face--front`}>{card.front}</div>
+                <div className={`card__face card__face--back`}>{card.back}</div>
             </div>
                 <div>
                     <span style={{textAlign:"left", width:"100%"}}>click card to show reverse</span>
